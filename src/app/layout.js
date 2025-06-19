6a79ee8e9a7213fa307fd2172fb03bc26d99c2d6
Umar Aftab <aftab.umar@yahoo.com>
2025-06-19 12:04:11 -0600

    Added Support for Analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Analytics } from '@vercel/analytics/next';
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,6 +29,7 @@ export default function RootLayout({ children }) {
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
+        <Analytics/>
       </body>
     </html>
   );
